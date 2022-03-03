29a7232d61a1163d63a1f927e8c3e535a2e032e7
GeneralBorN <bornabobohorina@gmail.com>
2022-03-03 19:30:51 +0100

    RADIIIII
    SIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII

--- BlankPage1.xaml.cs
+++ BlankPage1.xaml.cs
@@ -25,23 +25,23 @@ namespace Perojektni_Kviz
         //deklaracija varijabli
         int tocniOdgovor;
         int brojPitanja;
-        int score;
+        int score=0;
         int ukupnoPitanja;
-        
+        int trenutnoPitanje = 1;
+        int percentage;
+
         public BlankPage1()
         {
             this.InitializeComponent();
+
+            brojPitanja = 20;
+            postaviPitanje(trenutnoPitanje);
         }
         
         
         private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
         {
 
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(MainPage));
         }
         
         
@@ -50,11 +50,34 @@ namespace Perojektni_Kviz
             switch(qnum)//broj pitanja, te postavlja nova pitanja i tocne odgovore
             {
                 case 1://pitanje 1.
-
+                    pitanja.Text = "Tko je tocan odgovor?";
+                    tocniOdgovor = 1;
                     break;//zaustavlja switch
                 case 2://pitanje 2. itd
+                    pitanja.Text = "tako je!";
                     break;
             }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if(trenutnoPitanje==tocniOdgovor)
+            {
+                score++;
+            }
+
+            if(trenutnoPitanje==ukupnoPitanja)
+            {
+                percentage = (int)Math.Round((double)(score * 100) / ukupnoPitanja);
+                score = 0;
+                trenutnoPitanje = 0;
+            }
+            trenutnoPitanje++;
+            postaviPitanje(trenutnoPitanje);
         }
     }
 }
